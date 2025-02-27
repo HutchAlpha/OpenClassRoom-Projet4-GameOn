7b7c93a0f47b76ad589fb424698ea6b99cac7039
HutchAlpha <qboisset@hotmail.com>
2025-02-27 20:06:46 +0100

    Récupération infos formulaires

--- modal.js
+++ modal.js
@@ -1,34 +1,43 @@
-function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
-
-// DOM Elements
+//! Déclaration des DOMS
 const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+//! FIN Déclaration des DOMS
 
-// launch modal event
+//! Ouverture formulaire
+const modalBtn = document.querySelectorAll(".modal-btn");
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-// launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
+//!FIN Ouverture formulaire
 
-//Fermer formulaire
+//!Fermer formulaire
 const close = document.querySelector(".close");
 close.addEventListener("click", closed);
 
 function closed() {
   modalbg.style.display = "none";
 }
-//Fin Fermer formulaire
+//!Fin Fermer formulaire
 
-//traitement formulaire
+//!traitement formulaire
+const BoutonForm = document.querySelector(".btn-submit");
+BoutonForm.addEventListener("click", traitementFormulaire);
+  
+function traitementFormulaire(event){
+  event.preventDefault();
+  console.log("Le bouton du formulaire a été cliqué !");
+  // Récupération des données du formulaire
+  const prenom = document.getElementById("firstName").value;
+  const nom = document.getElementById("lastName").value;
+  const email = document.getElementById("email").value;
+  const birthdate = document.getElementById("birthdate").value;
+  const quantite = document.getElementById("quantityMatch").value;
+  const location = document.querySelector("input[name='location']:checked")?.value || "";
 
-//Fin traitement formulaire
+  console.log(`Prénom : ${prenom}, Nom : ${nom}, Email : ${email}, Date de naissance : ${birthdate}, Quantité : ${quantite}, Localisation : ${location}`);
+
+  // Vérification des données
+}
+//!Fin traitement formulaire
