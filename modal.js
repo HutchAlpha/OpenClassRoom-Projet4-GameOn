7157cd0efa33d6a193800246ab104ac23db4aa11
HutchAlpha <qboisset@hotmail.com>
2025-02-28 14:28:30 +0100

    Ajout fin formulaire + correction css

--- modal.js
+++ modal.js
@@ -1,7 +1,17 @@
 //! Déclaration des DOMS
 const modalbg = document.querySelector(".bground");
-const formData = document.querySelectorAll(".formData");
 //! FIN Déclaration des DOMS
+
+//!Burgur
+function editNav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+//!Fin Burgur
 
 //! Ouverture formulaire
 const modalBtn = document.querySelectorAll(".modal-btn");
@@ -22,8 +32,8 @@ function closed() {
 //!Fin Fermer formulaire
 
 //!traitement formulaire
-const BoutonForm = document.querySelector(".btn-submit");
-BoutonForm.addEventListener("click", traitementFormulaire);
+const BoutonForm = document.querySelector("form");
+BoutonForm.addEventListener("submit", traitementFormulaire);
   
 function traitementFormulaire(event){
   event.preventDefault();
@@ -40,8 +50,8 @@ function traitementFormulaire(event){
 
 
   console.log(`Prénom : ${prenom}, Nom : ${nom}, Email : ${email}, Date de naissance : ${birthdate}, Quantité : ${quantite}, Localisation : ${location}, conditions ${conditions}, informer ${informer}`);
+  
   // Vérification des données
-
   const form = document.querySelector("form"); 
 
   for (let i = form.length - 1; i >= 0; i--) {
@@ -50,19 +60,23 @@ function traitementFormulaire(event){
       alert(`Veuillez renseigner tous les champs du formulaire !`);
       return;
     }
-
     if (prenom.length < 2) {
       alert("Veuillez renseigner au moins 2 caractéres pour votre prénom");
       return;
     }
 
     if (nom.length < 2) {
-      alert("Veuillez renseigner au moins 2 caractéres pour votre nom");
+      alert("Veuillez entrer 2 caractères ou plus pour le champ du nom.");
       return;
     }
 
     if (!email.includes("@") ||!email.includes(".")) {
       alert("Veuillez renseigner un email valide");
+      return;
+    }
+
+    if (quantite.length === 0) {
+      alert("Veuillez renseigner une quantité de matchs");
       return;
     }
 
@@ -75,6 +89,41 @@ function traitementFormulaire(event){
       alert("Veuillez accepter les conditions d'utilisation");
       return;
     }
-}
+
+    //!Suppresion des données de formulaire
+    const questionTournoi = document.querySelector(".text-label");
+    if (questionTournoi) {
+      questionTournoi.style.display = "none";
+    }
+    //!FIN Suppresion des données de formulaire
+
+    //!Ajout message confirmation + bouton
+
+      //?Organisation du block comme sur le FIGMA
+      let content = document.querySelector(".content");
+      content.style.height = "100%";
+
+      const modalBody = document.querySelector(".modal-body");
+      modalBody.innerHTML = "";
+      modalBody.style.height = "100%";
+
+      let ajoutForm = document.createElement("p");
+      ajoutForm.textContent = "Merci pour votre inscription";
+      ajoutForm.style.textAlign = "center";
+      ajoutForm.style.fontSize = "36px";
+      //?FINOrganisation du block comme sur le FIGMA
+
+    let boutonRetour = document.createElement("button");
+    boutonRetour.textContent = "Retour";
+    boutonRetour.classList.add("btn-submit", "button");
+
+    boutonRetour.addEventListener("click", () => {
+      document.querySelector(".bground").style.display = "none";
+    });
+
+    modalBody.appendChild(ajoutForm);
+    modalBody.appendChild(boutonRetour);
+    //!FIN Ajout message confirmation + bouton
+  }
 }
 //!Fin traitement formulaire
